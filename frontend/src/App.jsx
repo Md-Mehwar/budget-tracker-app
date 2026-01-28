1d8e726c9cbc7c03df4007d26d9f8cfd8c09cf3f
athar-04 <athar.bng01@gmail.com>
2026-01-28 21:17:29 +0530

    Initial frontend setup with login and signup UI

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <div>
+      <h1>Budget Tracker App</h1>
+
+      <Login />
+      <hr />
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,19 @@
+function Signup() {
+  return (
+    <div>
+      <h2>Signup</h2>
+
+      <form>
+        <input type="text" placeholder="Name" />
+        <br /><br />
+        <input type="email" placeholder="Email" />
+        <br /><br />
+        <input type="password" placeholder="Password" />
+        <br /><br />
+        <button type="submit">Signup</button>
+      </form>
+    </div>
+  );
+}
+
+export default Signup;
